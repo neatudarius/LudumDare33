1b26cd022ef5c1d8874e3e0373a77bffd704f723
Razvan Brinzea <razvan.brinzea@gmail.com>
2015-08-22 21:35:30 +0300

    some stuff

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	void OnCollisionEnter2D( Collision2D coll ) {
+		if (coll.gameObject.name == "Player") {
+			Application.LoadLevel ("Menu");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
