f070ae583eb1366988f7f4db9a5b9834a8ee99bd
Razvan Brinzea <razvan.brinzea@gmail.com>
2015-08-22 22:56:44 +0300

    added an obstacle

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ObstacleSpawner : MonoBehaviour {
+
+	public List<GameObject> obstaclePrefabList;
+
+	private float spawnTime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +15,15 @@ public class ObstacleSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (spawnTime <= 0) {
+			int randomIndex = Random.Range ( 0, obstaclePrefabList.Count );
+			GameObject newObstacle = Instantiate (obstaclePrefabList[randomIndex]);
+			newObstacle.transform.parent = GameObject.Find ("ObstacleHolder").transform;
+			newObstacle.transform.position = transform.position;
+
+			spawnTime = Random.Range( 2.0f, 3.0f );
+		}
+
+		spawnTime = spawnTime - Time.deltaTime;
 	}
 }
