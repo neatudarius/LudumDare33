20c7483f1c04d17fd7a5e4ce39e0166228d392f0
Darius <neatudarius@yahoo.com>
2015-08-22 16:30:56 +0300

    Add menu system

---
+++ Assets/Scripts/UI/ButtonAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonAction : MonoBehaviour, IPointerUpHandler {
+    public Text buttonName;
+    public string command; 
+
+    #region IPointerUpHandler implementation
+   
+    public void OnPointerUp(PointerEventData eventData) {
+        if ( command != "" ) {
+            FindObjectOfType<MenuController> ( ).Command ( command );
+        }
+    }
+
+    #endregion
+
+     
+}
---
+++ Assets/Scripts/UI/StringsDatabase.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Collections;
+
+public class StringsDatabase : MonoBehaviour
+{
+    #region StringsDatabase's Things - DON'T CHANGE!!!
+	private static StringsDatabase _instance;
+
+	public static StringsDatabase instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<StringsDatabase> ();
+
+				//Tell unity not to destroy this object when loading a new scene!
+				DontDestroyOnLoad (_instance.gameObject);
+			}
+
+			return _instance;
+		}
+	}
+
+	void Awake ()
+	{
+		if (_instance == null) {
+			//If I am the first instance, make me the Singleton
+			_instance = this;
+			DontDestroyOnLoad (this);
+		} else {
+			//If a Singleton already exists and you find
+			//another reference in scene, destroy it!
+			if (this != _instance)
+				Destroy (this.gameObject);
+		}
+	}
+    #endregion
+
+    #region Titles, Names
+	public static string
+		gameName = "Baga aici",
+		menuSceneName = "Menu",
+		gameSceneName = "Game";
+
+    #endregion
+
+    #region Tags
+	public static string
+		__player = "Player";
+    #endregion
+
+    #region Buttons
+    public static string
+        _playGameButton = "Play",
+        _resumeGameButton = "Resume",
+        _backToMenuButton = "Back",
+        _creditsButton = "Credits",
+        _advanceToMenuScene = "Back to Main Menu";
+    #endregion
+
+    
+
+    public static string
+        screenShotName = "Screenshot";
+
+    
+
+}
