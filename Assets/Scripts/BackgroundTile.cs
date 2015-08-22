4f4a350da399e7c4ec7c17cedd3ba9f85f45830e
Razvan Brinzea <razvan.brinzea@gmail.com>
2015-08-22 16:49:42 +0300

    spawning road and background tiles

---
+++ Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundTile : MonoBehaviour {
+
+	private Vector2 leftCorner;
+	private Vector2 rightCorner;
+	private float width;
+
+	// Use this for initialization
+	void Awake () {
+		leftCorner = GetComponent<SpriteRenderer> ().bounds.min;
+		rightCorner = GetComponent<SpriteRenderer> ().bounds.max;
+		width = rightCorner.x - leftCorner.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public float GetWidth() {
+		return width;
+	}
+
+	public Vector2 GetCorner() {
+		return rightCorner;
+	}
+}
