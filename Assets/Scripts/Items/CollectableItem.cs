2dc8730e91de4203977c350396801d954d9b56a4
dumytru <nemtucdumitru@yahoo.com>
2015-08-23 18:30:17 +0300

    added sounds and bean anim

--- Assets/Scripts/Items/CollectableItem.cs
+++ Assets/Scripts/Items/CollectableItem.cs
@@ -7,13 +7,21 @@ public class CollectableItem : MonoBehaviour {
     public float rotateSpeed = 100.0f;
     private bool effectIsOn = false;
     public float timeUntilDestroy = 0.7f;
+    public AudioClip collectedSound;
 
+    private Animator anim;
+    private AudioSource audioSource;
 
     void Start ( ) {
         effectIsOn = false;
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("effectIsOn", false);
     }
     void OnTriggerEnter2D ( Collider2D hit ) {
         if ( !effectIsOn ) {
+            audioSource.PlayOneShot(collectedSound);
+            anim.SetBool("effectIsOn", true);
             gameObject.GetComponent<Collider2D> ( ).enabled = false;
             effectIsOn = true;
             GameObject.Find ( "RagePanel" ).GetComponent<RagePanelController> ( ).IncreaseRage ( );
@@ -22,10 +30,11 @@ public class CollectableItem : MonoBehaviour {
     }
 
     void Update ( ) {
+        /*
         if ( effectIsOn ) {
             gameObject.transform.Rotate ( Vector3.up, Time.deltaTime * rotateSpeed );
             gameObject.transform.Rotate ( Vector3.forward, Time.deltaTime * rotateSpeed );
-
         }
+        */
     }
 }
