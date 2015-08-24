a3d30ea79aaec8c3a4b6e1a1ba805de8a2042cd6
dumytru <nemtucdumitru@yahoo.com>
2015-08-24 12:12:24 +0300

    bigger explosion

--- Assets/Scripts/Obstacles/TNTExplosion.cs
+++ Assets/Scripts/Obstacles/TNTExplosion.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class TNTExplosion : MonoBehaviour {
 
+    public GameObject explosion;
+
 	void Start() {
-        transform.GetComponent<Renderer>().sortingLayerName = "Foreground";
+        explosion.GetComponent<Renderer>().sortingLayerName = "Foreground";
         Destroy(gameObject, 2.0f);
     }
 	
--- Assets/Scripts/Obstacles/TNTObstacle.cs
+++ Assets/Scripts/Obstacles/TNTObstacle.cs
@@ -7,7 +7,9 @@ public class TNTObstacle : Obstacle {
 
     public override void OnCollisionEnter2D(Collision2D coll) {
         base.OnCollisionEnter2D(coll);
-        if(explosionPrefab != null)
+        if (explosionPrefab != null) {
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            transform.GetComponent<Renderer>().enabled = false;
+        }
     }
 }
