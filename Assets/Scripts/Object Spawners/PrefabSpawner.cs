0e04e49016782d4d1a9e8633ae6e73564a18c88f
dumytru <nemtucdumitru@yahoo.com>
2015-08-25 00:24:20 +0300

    update and fix

--- Assets/Scripts/Object Spawners/PrefabSpawner.cs
+++ Assets/Scripts/Object Spawners/PrefabSpawner.cs
@@ -16,10 +16,10 @@ public class PrefabSpawner : MonoBehaviour {
 			
 			// Spawn a new prefab randomly from the list
 			int index = GlobalManager.rand ( 0, prefabs.Count-1);
-			GameObject newPrefab = Instantiate (prefabs[index]);
+			GameObject newPrefab = (GameObject)Instantiate (prefabs[index], transform.position, Quaternion.identity);
 			
 			// Align it to the last one
-			newPrefab.transform.position = transform.position; 			// Align it to the spawner
+			//newPrefab.transform.position = transform.position; 			// Align it to the spawner
 			float lastPrefabRight = lastPrefab.GetComponent<ScenePrefab> ().GetRightXValue();
 			float newPrefabLeft = newPrefab.GetComponent<ScenePrefab> ().GetLeftXValue();
 			float distance = newPrefabLeft - lastPrefabRight;			// Get the distance between the last prefab and the new prefab
