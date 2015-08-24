e50db24d8d8595a4b6270cd32025985bfefe8f01
dumytru <nemtucdumitru@yahoo.com>
2015-08-24 13:13:13 +0300

    added missile obstacle

--- Assets/Scripts/Obstacles/TNTExplosion.cs
+++ Assets/Scripts/Obstacles/TNTExplosion.cs
@@ -3,10 +3,7 @@ using System.Collections;
 
 public class TNTExplosion : MonoBehaviour {
 
-    public GameObject explosion;
-
 	void Start() {
-        explosion.GetComponent<Renderer>().sortingLayerName = "Foreground";
         Destroy(gameObject, 2.0f);
     }
 	
---
+++ Assets/Scripts/Utility/BringToForeground.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BringToForeground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().sortingLayerName = "Foreground";
+	}
+	
+}
