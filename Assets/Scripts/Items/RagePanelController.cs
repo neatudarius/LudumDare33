87d6b071f40f90c6efbc10c8eed4ba6c34f18165
Darius <neatudarius@yahoo.com>
2015-09-15 16:04:36 +0300

    Add second collider on player

--- Assets/Scripts/Items/RagePanelController.cs
+++ Assets/Scripts/Items/RagePanelController.cs
@@ -3,6 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityStandardAssets.ImageEffects;
 
+enum RageState
+{
+    Charging = 0,
+    Discharging = 1,
+    Full = 2
+}
+
 public class RagePanelController : MonoBehaviour {
     public float value {
         get {
@@ -21,6 +28,7 @@ public class RagePanelController : MonoBehaviour {
     public Text counter, comboDisplay;
     public GameObject displayText;
     public bool activated = false;
+    private RageState state;
     public float duration = 10f;
     public int incCoinsPeRage = 10;
 
@@ -35,7 +43,7 @@ public class RagePanelController : MonoBehaviour {
             comboDisplay.text = "Combo x" + _comboMultiply.ToString ( );
         }
     }
-    private float comboDuration = 10f;
+    public float comboDuration = 5f;
     private float resetComboTime = 0f;
 
     float t = 0f;
@@ -47,6 +55,7 @@ public class RagePanelController : MonoBehaviour {
         value = 0;
         total = 0;
         activated = false;
+        state = RageState.Charging;
         current = 0;
         comboMultiply = 1;
         resetComboTime = 0f;
@@ -61,15 +70,26 @@ public class RagePanelController : MonoBehaviour {
     
     public void IncreaseRage ( ) {
         total += comboMultiply;
-        current += comboMultiply;
         counter.text = total.ToString ( ) + " coins";
-        if ( !activated )
-            value = (float)current/needed*100f;
-                   
+       
+        
+
+        if ( state == RageState.Charging )
+        {
+            current += comboMultiply;
+            if ( current > needed )
+                current = needed;
+            value = ( float ) current / needed * 100f;
+        }
+
     }
 
     public void Activate ( ) {
         activated = true;
+        state = RageState.Discharging;
+        current = 0;
+        needed += incCoinsPeRage;
+        comboMultiply = 1;
         t = 0f;
         effect.enabled = true;
         GlobalManager.RageON ( );
@@ -77,23 +97,23 @@ public class RagePanelController : MonoBehaviour {
     }
 
     void Update ( ) {
-        if (Time.time > resetComboTime)
+        //Debug.Log ( current + "/"+ value + "/"+ needed + " remain: " +  (resetComboTime - Time.time) +"s");
+        if (Time.time > resetComboTime && comboMultiply > 1)
             comboMultiply = 1;
         if ( Ready ( ) ) {
             displayText.gameObject.SetActive (true );
         } else {
             displayText.gameObject.SetActive ( false );
         }
-        if ( activated ) {
+        if (state == RageState.Discharging ) {
             value = Mathf.Lerp ( 100f, 0f, t += Time.deltaTime / duration );
         }
     }
     void FixedUpdate ( ) {
         if ( activated && GlobalManager.progressBar.current < 0.1f ) {
             activated = false;
+            state = RageState.Charging;
             value = 0f;
-            current = 0;
-            needed += incCoinsPeRage;
             //displayText.gameObject.SetActive ( false );
             effect.enabled = false;
             GlobalManager.RageOFF ( );
@@ -111,7 +131,7 @@ public class RagePanelController : MonoBehaviour {
     public void Combo(int value)
     {
         comboMultiply++;
-        resetComboTime += comboDuration;
+        resetComboTime = Time.time + comboDuration;
         audioSource.PlayOneShot ( comboSound );
     }
 
